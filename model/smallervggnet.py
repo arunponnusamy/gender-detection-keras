6bf788e124e77ae316603e935fbbcd915f18d3b7
Arun Ponnusamy <arunponnusamy@hotmail.com>
2018-06-07 16:38:17 +0530

    Add model and training scripts

---
+++ model/smallervggnet.py
@@ -0,0 +1,56 @@
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dropout
+from keras.layers.core import Dense
+from keras import backend as K
+
+
+class SmallerVGGNet:
+    @staticmethod
+    def build(width, height, depth, classes):
+        model = Sequential()
+        inputShape = (height, width, depth)
+        chanDim = -1
+
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+            chanDim = 1
+
+        model.add(Conv2D(32, (3,3), padding="same", input_shape=inputShape))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(3,3)))
+        model.add(Dropout(0.25))
+
+        model.add(Conv2D(64, (3,3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(64, (3,3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2,2)))
+        model.add(Dropout(0.25))
+
+        model.add(Conv2D(128, (3,3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(128, (3,3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2,2)))
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(1024))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+
+        model.add(Dense(classes))
+        model.add(Activation("sigmoid"))
+
+        return model
---
+++ train.py
@@ -0,0 +1,99 @@
+import matplotlib
+matplotlib.use("Agg")
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras.preprocessing.image import img_to_array
+from keras.utils import to_categorical
+
+from sklearn.model_selection import train_test_split
+from model.smallervggnet import SmallerVGGNet
+import matplotlib.pyplot as plt
+
+import numpy as np
+import argparse
+import random
+import pickle
+import cv2
+import os
+import glob
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset (i.e., directory of images)")
+ap.add_argument("-m", "--model", type=str, default="gender_detection.model",
+	help="path to output model")
+ap.add_argument("-p", "--plot", type=str, default="plot.png",
+	help="path to output accuracy/loss plot")
+args = ap.parse_args()
+
+epochs = 100
+lr = 1e-3
+batch_size = 64
+img_dims = (96,96,3)
+
+data = []
+labels = []
+
+
+image_files = [f for f in glob.glob(args.dataset + "/**/*", recursive=True) if not os.path.isdir(f)] 
+random.seed(42)
+random.shuffle(image_files)
+
+for img in image_files:
+
+    image = cv2.imread(img)
+    
+    image = cv2.resize(image, (img_dims[0],img_dims[1]))
+    image = img_to_array(image)
+    data.append(image)
+
+    label = img.split(os.path.sep)[-2]
+    if label == "woman":
+        label = 1
+    else:
+        label = 0
+        
+    labels.append([label])
+
+data = np.array(data, dtype="float") / 255.0
+labels = np.array(labels)
+
+'''lb = LabelBinarizer()
+labels = lb.fit_transform(labels)'''
+
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.2,
+                                                  random_state=42)
+trainY = to_categorical(trainY, num_classes=2)
+testY = to_categorical(testY, num_classes=2)
+print(testY)
+print(testY.shape)
+aug = ImageDataGenerator(rotation_range=25, width_shift_range=0.1,
+                         height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
+                         horizontal_flip=True, fill_mode="nearest")
+
+model = SmallerVGGNet.build(width=img_dims[0], height=img_dims[1], depth=img_dims[2],
+                            classes=2)
+opt = Adam(lr=lr, decay=lr/epochs)
+model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
+
+H = model.fit_generator(aug.flow(trainX, trainY, batch_size=batch_size),
+                        validation_data=(testX,testY),
+                        steps_per_epoch=len(trainX) // batch_size,
+                        epochs=epochs, verbose=1)
+
+model.save(args.model)
+
+plt.style.use("ggplot")
+plt.figure()
+N = epochs
+plt.plot(np.arange(0,N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0,N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0,N), H.history["acc"], label="train_acc")
+plt.plot(np.arange(0,N), H.history["val_acc"], label="val_acc")
+
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="upper right")
+plt.savefig(args.plot)
